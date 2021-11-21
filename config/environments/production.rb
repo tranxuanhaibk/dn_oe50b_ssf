399475580c9b43e2fe04a2bc40b240df157edf05
tranxuanhaibk <tran.xuan.hai.bkdn@gmail.com>
2021-11-21 21:16:36 +0700

    Password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,30 +16,20 @@ Rails.application.configure do
   }
 
   config.cache_classes = true
-
   config.eager_load = true
-
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.compile = false
-
   config.active_storage.service = :local
-
   config.log_level = :info
   config.log_tags = [ :request_id ]
   config.action_mailer.perform_caching = false
-
   config.i18n.fallbacks = true
-
   config.active_support.deprecation = :notify
-
   config.active_support.disallowed_deprecation = :log
-
   config.active_support.disallowed_deprecation_warnings = []
-
   config.log_formatter = ::Logger::Formatter.new
-
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
@@ -48,5 +38,4 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
-
 end
