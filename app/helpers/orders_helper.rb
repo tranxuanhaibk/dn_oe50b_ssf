dfb65f8c9e002832a5d4017131b7f2a8949672cd
nguyenvanson332000 <nguyenvanson332000@gmail.com>
2021-12-07 00:56:40 -0800

    improve_view_handle_list_order_to_admin

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -24,4 +24,16 @@ module OrdersHelper
       "active"
     end
   end
+
+  def render_booking_button order, is_order_pending
+    if is_order_pending
+      text = t("booking.btn_handle")
+      class1 = "btn btn-danger"
+    else
+      text = t("booking.btn_view")
+      class1 = "btn btn-primary"
+    end
+    button_tag text, type: "button", class: class1,
+              data: {toggle: "modal", target: "#model#{order.id}"}
+  end
 end
