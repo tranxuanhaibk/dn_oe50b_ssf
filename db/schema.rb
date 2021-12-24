f0c9ad50979d076b4600eafc96a950cae4d1a7f7
tranxuanhaibk <tran.xuan.hai.bkdn@gmail.com>
2021-12-24 09:24:44 +0700

    Improve layout cart, project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_081648) do
+ActiveRecord::Schema.define(version: 2021_12_22_182143) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -20,6 +20,28 @@ ActiveRecord::Schema.define(version: 2021_12_14_081648) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["soccer_field_id"], name: "index_comments_on_soccer_field_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "target_type", null: false
+    t.integer "target_id", null: false
+    t.string "notifiable_type", null: false
+    t.integer "notifiable_id", null: false
+    t.string "key", null: false
+    t.string "group_type"
+    t.integer "group_id"
+    t.integer "group_owner_id"
+    t.string "notifier_type"
+    t.integer "notifier_id"
+    t.text "parameters"
+    t.datetime "opened_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_owner_id"], name: "index_notifications_on_group_owner_id"
+    t.index ["group_type", "group_id"], name: "index_notifications_on_group"
+    t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable"
+    t.index ["notifier_type", "notifier_id"], name: "index_notifications_on_notifier"
+    t.index ["target_type", "target_id"], name: "index_notifications_on_target"
   end
 
   create_table "order_details", force: :cascade do |t|
@@ -66,6 +88,24 @@ ActiveRecord::Schema.define(version: 2021_12_14_081648) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["soccer_field_id"], name: "index_soccer_rates_on_soccer_field_id"
     t.index ["user_id"], name: "index_soccer_rates_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "target_type", null: false
+    t.integer "target_id", null: false
+    t.string "key", null: false
+    t.boolean "subscribing", default: true, null: false
+    t.boolean "subscribing_to_email", default: true, null: false
+    t.datetime "subscribed_at"
+    t.datetime "unsubscribed_at"
+    t.datetime "subscribed_to_email_at"
+    t.datetime "unsubscribed_to_email_at"
+    t.text "optional_targets"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["key"], name: "index_subscriptions_on_key"
+    t.index ["target_type", "target_id", "key"], name: "index_subscriptions_on_target_type_and_target_id_and_key", unique: true
+    t.index ["target_type", "target_id"], name: "index_subscriptions_on_target"
   end
 
   create_table "users", force: :cascade do |t|
